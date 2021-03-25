d821d3aefe7eb32ef25dbb40d8528d632019d686
Ivo Ivanov <pifonsi@gmail.com>
2021-03-25 14:46:50 +0200

    Init

--- wpgulp.config.js
+++ wpgulp.config.js
@@ -7,63 +7,166 @@
  * @package WPGulp
  */
 
+// Project options.
+
+// Local project URL of your already running WordPress site.
+// > Could be something like "wpgulp.local" or "localhost"
+// > depending upon your local WordPress setup.
+const projectURL = 'spielgarten.local';
+
+// Theme/Plugin URL. Leave it like it is; since our gulpfile.js lives in the root folder.
+const productURL = './';
+const browserAutoOpen = false;
+const injectChanges = true;
+
+// >>>>> Style options.
+// Path to main .scss file.
+const styleSRC = './assets/scss/style.scss';
+
+// Path to place the compiled CSS file. Default set to root folder.
+const styleDestination = './assets/css/';
+
+// Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
+const outputStyle = 'compact';
+const errLogToConsole = true;
+const precision = 10;
+
+// JS Vendor options.
+
+// Path to JS vendor folder.
+const jsVendorSRC = './assets/js/vendor/*.js';
+
+// Path to place the compiled JS vendors file.
+const jsVendorDestination = './assets/js/';
+
+// Compiled JS vendors file name. Default set to vendors i.e. vendors.js.
+const jsVendorFile = 'vendor';
+
+// JS Custom options.
+
+// Path to JS custom scripts folder.
+const jsCustomSRC = './assets/js/custom/*.js';
+
+// Path to place the compiled JS custom scripts file.
+const jsCustomDestination = './assets/js/';
+
+// Compiled JS custom file name. Default set to custom i.e. custom.js.
+const jsCustomFile = 'main';
+
+// Images options.
+
+// Source folder of images which should be optimized and watched.
+// > You can also specify types e.g. raw/**.{png,jpg,gif} in the glob.
+const imgSRC = './assets/img/raw/**/*';
+
+// Destination folder of optimized images.
+// > Must be different from the imagesSRC folder.
+const imgDST = './assets/img/';
+
+// >>>>> Watch files paths.
+// Path to all *.scss files inside css folder and inside them.
+const watchStyles = './assets/scss/**/*.scss';
+
+// Path to all vendor JS files.
+const watchJsVendor = './assets/js/vendor/*.js';
+
+// Path to all custom JS files.
+const watchJsCustom = './assets/js/custom/*.js';
+
+// Path to all PHP files.
+const watchPhp = './**/*.php';
+
+// >>>>> Zip file config.
+// Must have.zip at the end.
+const zipName = 'file.zip';
+
+// Must be a folder outside of the zip folder.
+const zipDestination = './../'; // Default: Parent folder.
+const zipIncludeGlob = ['./**/*']; // Default: Include all files/folders in current directory.
+
+// Default ignored files and folders for the zip file.
+const zipIgnoreGlob = [
+	'!./{node_modules,node_modules/**/*}',
+	'!./.git',
+	'!./.svn',
+	'!./gulpfile.babel.js',
+	'!./wpgulp.config.js',
+	'!./.eslintrc.js',
+	'!./.eslintignore',
+	'!./.editorconfig',
+	'!./phpcs.xml.dist',
+	'!./vscode',
+	'!./package.json',
+	'!./package-lock.json',
+	'!./assets/css/**/*',
+	'!./assets/css',
+	'!./assets/img/raw/**/*',
+	'!./assets/img/raw',
+	`!${imgSRC}`,
+	`!${styleSRC}`,
+	`!${jsCustomSRC}`,
+	`!${jsVendorSRC}`
+];
+
+// >>>>> Translation options.
+// Your text domain here.
+const textDomain = 'SPIELGARTEN';
+
+// Name of the translation file.
+const translationFile = 'SPIELGARTEN.pot';
+
+// Where to save the translation files.
+const translationDestination = './languages';
+
+// Package name.
+const packageName = 'Spielgarten';
+
+// Where can users report bugs.
+const bugReport = 'https://github.com/ivo-ivanov';
+
+// Last translator Email ID.
+const lastTranslator = 'Ivo Ivanov';
+
+// Team's Email ID.
+const team = 'Ivo Ivanov';
+
+// Browsers you care about for auto-prefixing. Browserlist https://github.com/ai/browserslist
+// The following list is set as per WordPress requirements. Though; Feel free to change.
+const BROWSERS_LIST = ['last 2 version', '> 1%'];
+
+// Export.
 module.exports = {
-
-	// Project options.
-	projectURL: 'blanky.local', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
-	productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
-	browserAutoOpen: false,
-	injectChanges: true,
-
-	// Style options.
-	styleSRC: './assets/css/style.scss', // Path to main .scss file.
-	styleDestination: './', // Path to place the compiled CSS file. Default set to root folder.
-	outputStyle: 'compact', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
-	errLogToConsole: true,
-	precision: 10,
-
-	// JS Vendor options.
-	jsVendorSRC: './assets/js/vendor/*.js', // Path to JS vendor folder.
-	jsVendorDestination: './assets/js/', // Path to place the compiled JS vendors file.
-	jsVendorFile: 'vendor', // Compiled JS vendors file name. Default set to vendors i.e. vendors.js.
-
-	// JS Custom options.
-	jsCustomSRC: './assets/js/custom/*.js', // Path to JS custom scripts folder.
-	jsCustomDestination: './assets/js/', // Path to place the compiled JS custom scripts file.
-	jsCustomFile: 'custom', // Compiled JS custom file name. Default set to custom i.e. custom.js.
-
-	// Images options.
-	imgSRC: './assets/img/raw/*', // Source folder of images which should be optimized and watched. You can also specify types e.g. raw/**.{png,jpg,gif} in the glob.
-	imgDST: './assets/img/', // Destination folder of optimized images. Must be different from the imagesSRC folder.
-
-	// Watch files paths.
-	watchStyles: './assets/css/**/*.scss', // Path to all *.scss files inside css folder and inside them.
-	watchJsVendor: './assets/js/vendor/*.js', // Path to all vendor JS files.
-	watchJsCustom: './assets/js/custom/*.js', // Path to all custom JS files.
-	watchPhp: './**/*.php', // Path to all PHP files.
-
-	// Translation options.
-	textDomain: 'blanky', // Your textdomain here.
-	translationFile: 'blanky.pot', // Name of the translation file.
-	translationDestination: './languages', // Where to save the translation files.
-	packageName: 'blanky', // Package name.
-	bugReport: 'https://github.com/ivo-ivanov/', // Where can users report bugs.
-	lastTranslator: 'Ivo Ivanov <ivo@ivoivanov.net>', // Last translator Email ID.
-	team: 'webzino <hi@webzino.com>', // Team's Email ID.
-
-	// Browsers you care about for autoprefixing. Browserlist https://github.com/ai/browserslist
-	// The following list is set as per WordPress requirements. Though, Feel free to change.
-	BROWSERS_LIST: [
-		'last 2 version',
-		'> 1%',
-		'ie >= 11',
-		'last 1 Android versions',
-		'last 1 ChromeAndroid versions',
-		'last 2 Chrome versions',
-		'last 2 Firefox versions',
-		'last 2 Safari versions',
-		'last 2 iOS versions',
-		'last 2 Edge versions',
-		'last 2 Opera versions'
-	]
+	projectURL,
+	productURL,
+	browserAutoOpen,
+	injectChanges,
+	styleSRC,
+	styleDestination,
+	outputStyle,
+	errLogToConsole,
+	precision,
+	jsVendorSRC,
+	jsVendorDestination,
+	jsVendorFile,
+	jsCustomSRC,
+	jsCustomDestination,
+	jsCustomFile,
+	imgSRC,
+	imgDST,
+	watchStyles,
+	watchJsVendor,
+	watchJsCustom,
+	watchPhp,
+	zipName,
+	zipDestination,
+	zipIncludeGlob,
+	zipIgnoreGlob,
+	textDomain,
+	translationFile,
+	translationDestination,
+	packageName,
+	bugReport,
+	lastTranslator,
+	team,
+	BROWSERS_LIST
 };
